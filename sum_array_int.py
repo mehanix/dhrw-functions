566e38e88a5c102565775408e512dac8d85a0e36
Stefan Radu <30214214+Stefan-Radu@users.noreply.github.com>
2024-08-29 17:05:10 +0000

    Create sum_array_int.py
    take a list of integers and return their sum

---
+++ sum_array_int.py
@@ -0,0 +1,17 @@
+import json
+from enum import Enum
+
+from typing import Annotated, List
+
+from pydantic import BaseModel, Field
+from pydantic.config import ConfigDict
+
+
+class Input(BaseModel):
+    arr: List[str | int | None]
+
+class Output(BaseModel):
+    res: int
+
+def cleanup(input: Input) -> Output:
+    return Output(res=sum(input.arr))
