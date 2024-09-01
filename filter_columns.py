d85276806a2ad63ee578917509698c3a144aa038
Stefan Radu <30214214+Stefan-Radu@users.noreply.github.com>
2024-09-01 16:26:19 +0000

    Create filter_columns.py

---
+++ filter_columns.py
@@ -0,0 +1,22 @@
+import json
+from enum import Enum
+from typing import Annotated, List
+from pydantic import BaseModel, Field
+from pydantic.config import ConfigDict
+
+import pandas as pd
+from io import StringIO
+
+class Input(BaseModel):
+    csv_chunk: str
+
+class Output(BaseModel):
+    dataframe: pandas.core.frame.DataFrame
+
+def filter_columns(data: Input) -> Output:
+    COLUMNS = ['Date time', 'Temperature']
+    
+    csv_io = StringIO(data.csv_chunk)
+    df = pd.read_csv(csv_io)
+    df_filtered = df[COLUMNS]
+    return Output(dataframe=df_filtered)
